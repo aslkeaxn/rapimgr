60bf93672a96650cc7d1ffb386b0c71b78630015
aslkeaxn <69468068+aslkeaxn@users.noreply.github.com>
2024-01-20 10:19:16 +0300

    create account

--- src/register.ts
+++ src/register.ts
@@ -1,1 +1,75 @@
-export async function register() {}
+import puppeteer from "puppeteer";
+import { sleep } from "./sleep";
+import osecmail from "osecmail";
+
+export async function register(
+  username: string,
+  mask: string,
+  password: string,
+  login: string,
+  domain: string,
+  headless: false | "new" = "new"
+) {
+  const originalMessages = await osecmail.getMessages(login, domain);
+  const vmCounts = originalMessages.filter((m) =>
+    m.subject.includes("Verification Code")
+  ).length;
+
+  const browser = await puppeteer.launch({ headless });
+  const pages = await browser.pages();
+  const page = pages[0];
+
+  await page.goto("https://rapidapi.com/auth/sign-up");
+
+  await sleep(1000);
+  await page.locator("#signup-form_username").fill(username);
+  await sleep(1000);
+  await page.locator("#signup-form_email").fill(mask);
+  await sleep(1000);
+  await page.locator("#signup-form_password").fill(password);
+
+  const suButton = await page.waitForSelector("button span ::-p-text(Sign Up)");
+  if (!suButton) throw new Error("Couldn't find sign up button");
+
+  await sleep(2000);
+
+  let i = 0;
+  let url = page.url();
+  while (i < 10 && page.url() === url) {
+    i++;
+    await suButton.click();
+    await sleep(5000);
+  }
+
+  if (page.url() === url) throw new Error("Couldn't proceed with sign up");
+
+  let j = 0;
+  let code = "";
+  while (j < 10) {
+    j++;
+    await sleep(3000);
+    const messages = await osecmail.getMessages(login, domain);
+    const vMessages = messages.filter((m) =>
+      m.subject.includes("Verification Code")
+    );
+    if (vMessages.length === vmCounts) continue;
+    code = vMessages[0].subject.split(" ")[2];
+    break;
+  }
+
+  if (code.length === 0) throw new Error("Couldn't get verification code");
+
+  url = page.url();
+  await page.locator("input").fill(code);
+
+  let k = 0;
+  while (k < 10 && url === page.url()) {
+    k++;
+    await sleep(1000);
+  }
+
+  if (url === page.url()) throw new Error("Couldn't verify account");
+
+  await page.close();
+  await browser.close();
+}
